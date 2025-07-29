c3b074aaef4be179ecf912237fa3e578235ce9b7
AndresDev20 <andresmarroquin887@gmail.com>
2025-07-29 16:11:49 -0500

    Aprendido Django

--- Backend/Django/clase_4/trabajando_con_datos/users/urls.py
+++ Backend/Django/clase_4/trabajando_con_datos/users/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='index'),
+    path('create/', views.createUser, name='create_user'),
+    path('delete/<int:user_id>/', views.deleteUser, name='delete_user'),
 ]
--- Backend/Django/clase_4/trabajando_con_datos/users/views.py
+++ Backend/Django/clase_4/trabajando_con_datos/users/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import User
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+def createUser(request):
+    # Primera forma de principiante
+    #user = User(firstname="Yeison", lastname="Andres", email="yeison.andres@example.com", password="securepassword", edad=20)
+    #user.save()
+
+    # Segunda forma de principiante
+    user = User.objects.create(firstname="Alisson", lastname="Veronica", email="alisson.veronica@example.com", password="securepassword", edad=2)
+    return HttpResponse(f"User {user.firstname} {user.lastname} created successfully!")
+
+def deleteUser(request, user_id):
+    user = User.objects.get(id=user_id)
+    user.delete()
+    return HttpResponse(f"User {user.firstname} {user.lastname} deleted successfully!")
+    
