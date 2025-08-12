303af74ffb0e795a264bdd943edffa55847b15a4
AndresDev20 <andresmarroquin887@gmail.com>
2025-08-12 14:24:13 -0500

    Aprendiendo Django

--- Backend/Django/clase_6/project_practice/app/admin.py
+++ Backend/Django/clase_6/project_practice/app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Empleado, Job, Salary, Place, Location, Country
 
 # Register your models here.
+admin.site.register(Empleado)
+admin.site.register(Job)
+admin.site.register(Salary)
+admin.site.register(Place)
+admin.site.register(Location)
+admin.site.register(Country)
