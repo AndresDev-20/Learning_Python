1054e580545a0f55d08a93dc66a2ae3643561c09
AndresDev20 <andresmarroquin887@gmail.com>
2025-08-14 13:00:27 -0500

    Aprendiendo Django

--- Backend/Django/clase_7/formularios_django/formularios/forms.py
+++ Backend/Django/clase_7/formularios_django/formularios/forms.py
@@ -2,6 +2,6 @@ from django import forms
 
 
 class MyForm(forms.Form):
-    name = forms.CharField(label='Your name', max_length=100)
-    email = forms.EmailField(label='Your email', initial='example@example.com')
-    message = forms.CharField(label='Message')
+    name = forms.CharField(label='Your name', max_length=100, widget=forms.TextInput(attrs={'placeholder': 'Enter your name', "class": "form-control"}))
+    email = forms.EmailField(label='Your email', initial='example@example.com', widget=forms.EmailInput(attrs={'placeholder': 'Enter your email', "class": "form-control"}))
+    message = forms.CharField(label='Message', widget=forms.Textarea(attrs={'placeholder': 'Enter your message', "class": "form-control"}))
