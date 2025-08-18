5c8ad737e6175807eb0d45770dd72a51210228af
AndresDev20 <andresmarroquin887@gmail.com>
2025-08-18 18:24:27 -0500

    Aprendiendo Django

---
+++ Backend/Django/clase_8/modelsforms/employees/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class Employee(models.Model):
+    first_name = models.CharField(max_length=50, blank=False, null=False)
+    last_name = models.CharField(max_length=50, blank=False, null=False)
+    email = models.EmailField(blank=False, null=False)
+    phone = models.CharField(max_length=15, blank=False, null=False)
+    hire_date = models.DateField(blank=False, null=False)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
