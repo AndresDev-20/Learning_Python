1ac9e0e7fc6eebe16abd007804abb680c56060b6
AndresDev20 <andresmarroquin887@gmail.com>
2025-08-19 18:25:17 -0500

    Aprendiendo Django

--- Backend/Django/clase_8/practice_forms/stock/models.py
+++ Backend/Django/clase_8/practice_forms/stock/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=50, blank=False, null=False)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2, blank=False, null=False)
+    stock = models.IntegerField(default=20)
+
+    def __str__(self):
+       return self.name
