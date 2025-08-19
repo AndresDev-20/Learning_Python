69c8dd9fd7ca1169d03a80d6881904a100bc6510
AndresDev20 <andresmarroquin887@gmail.com>
2025-08-18 19:11:41 -0500

    Aprendiendo Django

--- Backend/Django/clase_8/modelsforms/employees/urls.py
+++ Backend/Django/clase_8/modelsforms/employees/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='employee_index'),
+    path('success/', views.success, name='employee_success'),
 ]
--- Backend/Django/clase_8/modelsforms/employees/views.py
+++ Backend/Django/clase_8/modelsforms/employees/views.py
@@ -1,8 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .forms import EmployeeForm
 
 # Create your views here.
 def index(request):
-    form = EmployeeForm();
+    if request.method == 'POST':
+        form = EmployeeForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('employee_success')
+    else:
+        form = EmployeeForm()
     return render(request, 'index.html', {'form': form})
+
+
+def success(request):
+    return render(request, 'success.html')
