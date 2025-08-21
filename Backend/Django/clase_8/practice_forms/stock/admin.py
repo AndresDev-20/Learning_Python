a03a9d5b899441f48b6f5088d1fb357c0da5e651
AndresDev20 <andresmarroquin887@gmail.com>
2025-08-21 10:58:19 -0500

    Aprendido django

--- Backend/Django/clase_8/practice_forms/stock/admin.py
+++ Backend/Django/clase_8/practice_forms/stock/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from .models import Product
 
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'stock')
+    search_fields = ('name',)
+    list_filter = ('stock', 'price')
+
 # Register your models here.
-admin.site.register(Product)
+admin.site.register(Product, ProductAdmin)
